fa21290db452be70886b6acdd43594cb254de617
Andy <mcho588@github.com>
2015-08-06 11:55:21 +1200

    got single script to run everything

--- runworld.bash
+++ runworld.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-rosrun stage_ros stageros world/myworld.world
+gnome-terminal -x bash -c 'roscore'
+
+gnome-terminal -x bash -c 'rosrun stage_ros stageros world/myworld.world'
+
+sleep 3
+
+gnome-terminal -x bash -c 'bash use-launch.bash'
+#rosrun stage_ros stageros world/myworld.world
---
+++ use-launch.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ugbots_ros
+
+rosmake
+
+sleep 5
+
+roslaunch ugbots_ros example.launch
