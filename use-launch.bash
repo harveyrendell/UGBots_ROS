9f04bf14926b79470ab30004ba19197fabaa3be7
Andy <mcho588@github.com>
2015-08-06 13:17:39 +1200

    commented bash scripts and modifed slightly

--- use-launch.bash
+++ use-launch.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+#move into project directory
 cd ugbots_ros
 
+#build before running any robots
 rosmake
 
-sleep 5
-
+#launch the example launch file
+#change launch file name to launch file you want to use
 roslaunch ugbots_ros example.launch
+
---
+++ use-node.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#move into project directory
+cd ugbots_ros
+
+#build before running any robots
+rosmake
+
+#run the node you want
+#rosrun ugbots_ros <node name>
