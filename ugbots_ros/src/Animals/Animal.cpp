16535f1533e3f190c9a1bc18c66751b20cf9f20c
namjun <https://github.com/namj>
2015-08-07 17:30:20 +1200

    Modified animal class, copied some (correct) code from CarrierBot class.

--- ugbots_ros/src/Animals/Animal.cpp
+++ ugbots_ros/src/Animals/Animal.cpp
@@ -17,16 +17,17 @@ public:
 		this->n = n;
 
 		//setting base attribute defaults
-		pose.theta = M_PI/2.0;
-		pose.px = 10;
-		pose.py = 20;
-		speed.linear_x = 0.0;
-		speed.max_linear_x = 3.0;
-		speed.angular_z = 20.0;
+		this->pose.theta = M_PI/2.0;
+		this->pose.px = 10;
+		this->pose.py = 20;
+		this->speed.linear_x = 0.0;
+		this->speed.max_linear_x = 3.0;
+		this->speed.angular_z = 20.0;
 
-		sub_list.node_stage_pubb = n.advertise<geometry_msgs::Twist>("robot_0/cmd_vel",1000);
-		sub_list.sub_odom = n.subscribe<nav_msgs::Odometry>("robot_0/odom",1000, &Template::odom_callback, this);
-		sub_list.sub_laser = n.subscribe<sensor_msgs::LaserScan>("robot_0/base_scan",1000,&Template::laser_callback, this);
+		this->sub_list.node_stage_pub = n.advertise<geometry_msgs::Twist>("robot_0/cmd_vel",1000);
+		this->sub_list.sub_odom = n.subscribe<nav_msgs::Odometry>("robot_0/odom",1000, &Template::odom_callback, this);
+		this->sub_list.sub_laser = n.subscribe<sensor_msgs::LaserScan>("robot_0/base_scan",1000,&Template::laser_callback, this);
+
 	}
 
 	virtual void moveTo(int x, int y){
@@ -36,10 +37,10 @@ public:
 	void odom_callback(nav_msgs::Odometry msg)
 	{
 		//This is the call back function to process odometry messages coming from Stage. 	
-		px = 5 + msg.pose.pose.position.x;
-		py = 10 + msg.pose.pose.position.y;
-		ROS_INFO("Current x position is: %f", px);
-		ROS_INFO("Current y position is: %f", py);
+		this->pose.px = 5 + msg.pose.pose.position.x;
+		this->pose.py = 10 + msg.pose.pose.position.y;
+		ROS_INFO("Current x position is: %f", this->pose.px);
+		ROS_INFO("Current y position is: %f", this->pose.py);
 	}
 
 
@@ -50,3 +51,52 @@ public:
 		
 	}
 };
+
+int main(int argc, char **argv)
+{	
+//You must call ros::init() first of all. ros::init() function needs to see argc and argv. The third argument is the name of the node
+ros::init(argc, argv, "CB");
+
+//NodeHandle is the main access point to communicate with ros.
+ros::NodeHandle n;
+
+CarrierBot cb(n);
+
+/*//advertise() function will tell ROS that you want to publish on a given topic_
+//to stage
+//ros::Publisher RobotNode_stage_pub = n.advertise<geometry_msgs::Twist>("robot_0/cmd_vel",1000); 
+//use the one below when using launch, use the one above when testing individual robot
+ros::Publisher RobotNode_stage_pub = n.advertise<geometry_msgs::Twist>("cmd_vel",1000); 
+
+//subscribe to listen to messages coming from stage
+//robo.setSubs(n);
+ros::Subscriber StageOdo_sub = n.subscribe<nav_msgs::Odometry>("robot_0/odom",1000, &R0::StageOdom_callback, &robo);
+ros::Subscriber StageLaser_sub = n.subscribe<sensor_msgs::LaserScan>("robot_0/base_scan",1000,&R0::StageLaser_callback, &robo);*/
+
+
+ros::Rate loop_rate(10);
+
+//a count of howmany messages we have sent
+int count = 0;
+
+////messages
+//velocity of this RobotNode
+geometry_msgs::Twist RobotNode_cmdvel;
+
+while (ros::ok())
+{
+	//messages to stage
+	RobotNode_cmdvel.linear.x = cb.speed.linear_x;
+	RobotNode_cmdvel.angular.z = cb.speed.angular_z;
+        
+	//publish the message
+	cb.sub_list.node_stage_pub.publish(RobotNode_cmdvel);
+	
+	ros::spinOnce();
+
+	loop_rate.sleep();
+	++count;
+}
+return 0;
+
+}
