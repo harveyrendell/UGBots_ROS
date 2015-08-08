c78e4dff2e26b25323552a0b94b75f153cfe7a4c
Harvey <hjrendell@gmail.com>
2015-08-08 19:47:12 +1200

    Added a basic test with its own launcher as an example

---
+++ ugbots_ros/test/unit_tests.cpp
@@ -0,0 +1,14 @@
+// Bring in gtest
+#include <gtest/gtest.h>
+
+// Declare a test
+TEST(TestSuite, testCase1)
+{
+	EXPECT_TRUE(true);
+}
+
+// Run all the tests that were declared with TEST()
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
