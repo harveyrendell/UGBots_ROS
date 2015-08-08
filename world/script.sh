db8dd4337ebd1bd9877a72863180ac17ce0eefa7
blee660 <bylee94@hotmail.com>
2015-08-08 13:12:33 +1200

    inc files made to be generated when script file is run (Dimensions of kiwifruit rows not yet correct)

--- world/script.sh
+++ world/script.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
  
-echo -e "Number of kiwifruit rows to be generated: \c "
-read  kiwi
+echo "Number of kiwifruit rows to be generated: \c "
+read  rows
 
-echo -e "Number of picker robots to be generated: \c "
+echo "Number of picker robots to be generated: \c "
 read picker
 
-echo -e "Number of carrier robots to be generated: \c "
+echo "Number of carrier robots to be generated: \c "
 read carrier
+
+rm worldinstances.inc
+
+echo include \"\kiwirow.inc\"\ > worldinstances.inc
+
+i=0
+j=0
+
+while [ $i -lt $rows ];
+do
+k=$((-20+$j))
+echo rows\(\pose [ $k 20 0 0 ]\)\ >> worldinstances.inc
+i=$(($i+1))
+j=$((8+$j))
+
+done
 
 rosrun stage_ros stageros ./myworld.world
