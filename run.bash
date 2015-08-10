833dd0876e509cc4505d29d0b0902a8b135cc592
namjun <https://github.com/namj>
2015-08-10 22:19:27 +1200

    fixed random spelling error

--- run.bash
+++ run.bash
@@ -16,4 +16,4 @@ gnome-terminal -x bash -c 'rosrun stage_ros stageros world/myworld.world'
 #FOR INDIVIDUAL ROSRUN
 #Open terminal to run script that runs an individual node
 #REMEMBER to edit the use-node.bash script to run the node you want to run
-rocgnome-terminal -x bash -c 'bash use-node.bash'
+gnome-terminal -x bash -c 'bash use-node.bash'
