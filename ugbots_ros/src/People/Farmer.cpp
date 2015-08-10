54e030b64c17a9421beacd9f1e0a623d7c37f0d5
Jay <jkim506@aucklanduni.ac.nz>
2015-08-10 13:25:00 +1200

    Turns 90 degrees for positive radian sector, calculated clock rate for preemtive radian definition

--- ugbots_ros/src/People/Farmer.cpp
+++ ugbots_ros/src/People/Farmer.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "tf/tf.h"
 #include <geometry_msgs/Twist.h>
 #include <nav_msgs/Odometry.h>
 #include <sensor_msgs/LaserScan.h>
@@ -7,10 +8,23 @@
 #include <sstream>
 #include <iostream>
 #include <stdlib.h>
+#include <math.h>
 #include "../Headers/Unit.h"
 
 class Farmer : public Unit
 {
+private:
+	int previous_right_distance;
+	int previous_left_distance;
+	int previous_front_distance;
+	bool currently_turning;
+
+	double rotx;
+	double roty;
+	double rotz;
+	double rotw;
+	double angle;
+	double desired_angle;
 public:
 	Farmer(ros::NodeHandle &n)
 	{
@@ -23,6 +37,13 @@ public:
 		this->speed.linear_x = 20.0;
 		this->speed.max_linear_x = 30.0;
 		this->speed.angular_z = 0.0;
+
+		this->previous_right_distance = 0;
+		this->previous_left_distance = 0;
+		this->previous_front_distance = 0;
+		this->angle = 0;
+		this->desired_angle = M_PI / 2;
+		this->currently_turning = false;
 
 		this->sub_list.node_stage_pub = n.advertise<geometry_msgs::Twist>("robot_1/cmd_vel",1000);
 		this->sub_list.sub_odom = n.subscribe<nav_msgs::Odometry>("robot_1/odom",1000, &Farmer::odom_callback, this);
@@ -38,7 +59,12 @@ public:
 		//This is the call back function to process odometry messages coming from Stage. 	
 		this->pose.px = 5 + msg.pose.pose.position.x;
 		this->pose.py = 10 + msg.pose.pose.position.y;
-		//ROS_INFO("Current x position is: %f", this->pose.px);
+		this->rotx = msg.pose.pose.orientation.x;
+    		this->roty = msg.pose.pose.orientation.y;
+    		this->rotz = msg.pose.pose.orientation.z;
+    		this->rotw = msg.pose.pose.orientation.w;
+		this->angle = atan2(2*(roty*rotx+rotw*rotz),rotw*rotw+rotx*rotx-roty*roty-rotz*rotz);
+		
 		//ROS_INFO("Current y position is: %f", this->pose.py);	
 	}
 
@@ -47,26 +73,38 @@ public:
 		//This is the callback function to process laser scan messages
 		//you can access the range data from msg.ranges[i]. i = sample number
 
-		if(msg.ranges[90] < 3.0 || msg.ranges[60] < 3.0 ||  msg.ranges[120] < 3.0 )
+		if(this->currently_turning)
 		{
-			if(msg.ranges[60] >= msg.ranges [90])
-			{
-				this->speed.angular_z = 150;
-			}
-			else
-			{
-				this->speed.angular_z = -150;
-			}
+			ROS_INFO("Current angle is: %f", this->angle);		
+			ROS_INFO("Current next desired angle is: %f", this->desired_angle);
+			//ROS_INFO("Test laser: TURNING RIGHT %f", msg.ranges[0]);
 
-			this->speed.linear_x = 0.0;
-
-			ROS_INFO("Test laser: %f", msg.ranges[90]);	
+			//2 clocks
+			if((this->angle + 0.31416) >= this->desired_angle)
+			{
+				this->currently_turning = false;
+				this->speed.linear_x = 20.0;
+				this->speed.angular_z = 0.0;
+				this->desired_angle = this->desired_angle + M_PI / 2.000000;
+			}
+			return;
 		}
-		else
+		
+		
+		if(msg.ranges[90] < 3.0)
 		{
-			this->speed.linear_x = 20.0;
-			this->speed.angular_z = 0.0;
-		}				
+			this->currently_turning = true;
+
+			this->previous_right_distance = msg.ranges[0];
+			this->previous_left_distance = msg.ranges[180];
+			this->previous_front_distance = msg.ranges[90];
+			
+			this->speed.linear_x = 0.0;
+			this->speed.angular_z = 5.0;
+
+			//ROS_INFO("Test laser: FRONT %f", msg.ranges[90]);	
+		}	
+					
 	}
 };
 
