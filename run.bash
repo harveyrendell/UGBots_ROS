1660dbb59169c1068c29c08071a9a1ac032585b3
chuan1994 <cwu323@aucklanduni.ac.nz>
2015-08-10 13:01:15 +1200

    run script runs debugger (rqt)

--- run.bash
+++ run.bash
@@ -13,6 +13,8 @@ gnome-terminal -x bash -c 'rosrun stage_ros stageros world/myworld.world'
 #Open terminal to run script that runs the example launch file
 gnome-terminal -x bash -c 'bash use-launch.bash'
 
+
+gnome-terminal -x bash -c 'rosrun rqt_console rqt_console'
 #FOR INDIVIDUAL ROSRUN
 #Open terminal to run script that runs an individual node
 #REMEMBER to edit the use-node.bash script to run the node you want to run
