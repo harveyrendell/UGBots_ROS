1ab3257f454bc84e0fcfd258ec560c98071e530d
chuan1994 <cwu323@aucklanduni.ac.nz>
2015-08-10 12:00:45 +1200

    record script works

--- record.bash
+++ record.bash
@@ -4,12 +4,28 @@
 mkdir ./bagfiles
 cd bagfiles
 
-echo "Enter in seconds how long you would like to record for"
-read seconds
+read -p "would you like to record session?" choice
 
-#Begin recording and kill after n seconds
+case "$choice" in
+	y|Y)
+	echo "Enter in seconds how long you would like to record for: "
+	read seconds
 
-( cmdpid=$BASHPID; (sleep "$seconds"; kill -TERM $cmdpid) & exec rosbag record -a )
+	echo "Enter name of output file: "
+	read name
 
-cd ..
+	#Begin recording and kill after n seconds
 
+	( cmdpid=$BASHPID; (sleep $seconds; kill -INT $cmdpid) & exec rosbag record -a -O $name)
+	pkill rosbag
+	pkill record
+		
+
+	rosbag reindex $name.*
+
+
+	rm *.orig.*;;
+	
+	
+
+esac
