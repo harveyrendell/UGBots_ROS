ae20df3e6f010d7a4cbfc1c0ff54b44f46402363
Harvey <hjrendell@gmail.com>
2015-08-11 12:41:11 +1200

    Finished refactoring code

---
+++ ugbots_ros/include/node_defs/possum.h
@@ -0,0 +1,22 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/LaserScan.h>
+
+#include <sstream>
+#include <stdlib.h>
+#include <node.h>
+
+class Possum : public Node
+{
+public:
+	Possum(ros::NodeHandle &n);
+	void odom_callback(nav_msgs::Odometry msg);
+	void laser_callback(sensor_msgs::LaserScan msg);
+	void move();
+	void stop();
+	void turnLeft();
+	void turnRight();
+	void collisionDetected();
+};
