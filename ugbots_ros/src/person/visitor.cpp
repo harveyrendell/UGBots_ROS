d73d7d77f2133e42319b7ca6cba18967d4b58969
Jay <jkim506@aucklanduni.ac.nz>
2015-08-21 18:23:01 +1200

    commit for local changes

--- ugbots_ros/src/person/visitor.cpp
+++ ugbots_ros/src/person/visitor.cpp
@@ -42,6 +42,8 @@ Visitor::Visitor(ros::NodeHandle &n)
 	this->orientation.currently_turning = false;
 	this->orientation.currently_turning_static = false;
 
+	this->temp_for_testing = false;
+
 	this->sub_list.node_stage_pub = n.advertise<geometry_msgs::Twist>("cmd_vel",1000);
 	this->sub_list.sub_odom = n.subscribe<nav_msgs::Odometry>("odom",1000, &Visitor::odom_callback, this);
 	this->sub_list.sub_laser = n.subscribe<sensor_msgs::LaserScan>("base_scan",1000,&Visitor::laser_callback, this);
@@ -57,11 +59,20 @@ void Visitor::odom_callback(nav_msgs::Odometry msg)
 	this->orientation.rotz = msg.pose.pose.orientation.z;
 	this->orientation.rotw = msg.pose.pose.orientation.w;
 	
+
 	calculateOrientation();
+
+	if(this->temp_for_testing == false)
+	{
+		this->orientation.desired_angle = this->orientation.angle;
+		this->temp_for_testing = true;
+	}
 
 	doAngleCheck();		
 
 	checkTurningStatus();
+
+	publish();
 
 	//checkStaticTurningStatus();
 
@@ -69,22 +80,43 @@ void Visitor::odom_callback(nav_msgs::Odometry msg)
 	ROS_INFO("/position/y/%f",this->pose.py);
 	ROS_INFO("/status/%s/./",enum_to_string(this->state));*/	
 
-	ROS_INFO("ANGLE: %f",this->orientation.angle);
-	ROS_INFO("DESIRED ANGLE: %f", this->orientation.desired_angle);
+	//ROS_INFO("ANGLE: %f",this->orientation.angle);
+	//ROS_INFO("DESIRED ANGLE: %f", this->orientation.desired_angle);
 
 }
 
 
 void Visitor::laser_callback(sensor_msgs::LaserScan msg)
 {
-	if(msg.ranges[90] < 3.5)
+	if(msg.ranges[90] < 3.0)
 	{
-		turn(M_PI / 2, 2.0, 5.0);
+		ROS_INFO("ANGLE: %f",this->orientation.angle);
+		ROS_INFO("DESIRED ANGLE: %f", this->orientation.desired_angle);
+		turn((M_PI) , 0.0, -5.0);
 
 	}
 
 	//This is the callback function to process laser scan messages
 	//you can access the range data from msg.ranges[i]. i = sample number	
+}
+
+void Visitor::checkTurningStatus()
+{
+		//Implement individually.
+		//Change 2-3 to which ever suits your node
+		//parse in ur desired linear speed to stopTurn()
+		
+	if(this->orientation.currently_turning == true)
+	{
+		if(this->orientation.angle  == this->orientation.desired_angle)
+		{
+			this->orientation.currently_turning = false;
+			this->speed.linear_x = 2.0;
+			this->speed.angular_z = 0.0; 	
+		}
+		return;
+	}
+		
 }
 
 void Visitor::move(){}
@@ -114,7 +146,6 @@ int count = 0;
 
 while (ros::ok())
 {
-	node.publish();
 	
 	ros::spinOnce();
 
